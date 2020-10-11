e094a5cffd0c7ab068fe5229ddfd7d97ebba44f1
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-10-11 23:38:17 +0200

    Takes pictures and merge them

--- upload.php
+++ upload.php
@@ -1,17 +1,43 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+//Fileupload
+if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile'])){
+	
+	  $file_name_main = $_FILES['mainFile']['name'];
+      $file_tmp_main =$_FILES['mainFile']['tmp_name'];
+	  
+	  $file_name_copyright = $_FILES['copyrightFile']['name'];
+      $file_tmp_copyright =$_FILES['copyrightFile']['tmp_name'];
+	  
+	  move_uploaded_file($file_tmp_main,"uploads/".$file_name_main);
+	  move_uploaded_file($file_tmp_copyright,"uploads/".$file_name_copyright);
 }
+
+//calculate new size
+$percent = 0.3;
+
+$mainfilePath = "uploads/".$file_name_main;
+$stampResize = "uploads/".$file_name_copyright;
+
+$image = imagecreatefromjpeg("uploads/".$file_name_main);
+$stamp = imagecreatefromjpeg("uploads/".$file_name_copyright);
+
+list($width, $height) = getimagesize($stampResize);
+$newwidth = $width * $percent;
+$newheight = $height * $percent;
+
+$thumb = imagecreatetruecolor($newwidth, $newheight);
+
+imagecopyresized($thumb, $stamp, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+$x = imagesx($thumb);
+$y = imagesy($thumb);
+
+//Put watermark on image
+imagecopymerge($image, $thumb, imagesx($image) - $x, imagesy($image) - $y, 0, 0, imagesx($thumb), imagesy($thumb), 50);
+
+imagepng($image, 'uploads/photo_thumb.png');
+
+unlink($mainfilePath);
+unlink($stampResize);
+
 ?>
