9734d13fb9f30b6f9f352b0ed579929ba8c0a0d2
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-10-15 21:55:02 +0200

    Image download (not functional)

--- upload.php
+++ upload.php
@@ -1,16 +1,16 @@
 <?php
 //Fileupload
-if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile'])){
+if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile']['error'] == UPLOAD_ERR_OK)){
+	$resultfilename = "resultImageTmp";
 	
 	  $file_name_main = $_FILES['mainFile']['name'];
-      $file_tmp_main =$_FILES['mainFile']['tmp_name'];
+	  $file_tmp_main =$_FILES['mainFile']['tmp_name'];
 	  
 	  $file_name_copyright = $_FILES['copyrightFile']['name'];
-      $file_tmp_copyright =$_FILES['copyrightFile']['tmp_name'];
+	  $file_tmp_copyright =$_FILES['copyrightFile']['tmp_name'];	  
 	  
 	  move_uploaded_file($file_tmp_main,"uploads/".$file_name_main);
 	  move_uploaded_file($file_tmp_copyright,"uploads/".$file_name_copyright);
-}
 
 //calculate new size
 $percent = 0.3;
@@ -35,9 +35,18 @@ $y = imagesy($thumb);
 //Put watermark on image
 imagecopymerge($image, $thumb, imagesx($image) - $x, imagesy($image) - $y, 0, 0, imagesx($thumb), imagesy($thumb), 50);
 
-imagepng($image, 'uploads/photo_thumb.png');
+imagejpeg($image, 'uploads/'.$resultfilename.'.jpg');
 
 unlink($mainfilePath);
 unlink($stampResize);
+
+$resultfilePath = "uploads/".$resultfilename.'jpg';
+    
+	echo '<div class="img-box">';
+		echo '<img src="' . $resultImage . '" width="200" alt="' .  pathinfo($resultfilePath, PATHINFO_FILENAME) .'">';
+		echo '<p><a href="download.php?file=' . urlencode($resultfilePath) . '">Download Image</a></p>';
+	echo '</div>';
+
+}
 
 ?>
