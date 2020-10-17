256805273acf32407483a96560702348ed4b9a88
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-10-18 00:50:39 +0200

    working converting

--- upload.php
+++ upload.php
@@ -1,7 +1,9 @@
 <?php
 //Is not file empty load picutres and save it
 if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile']['error'] == UPLOAD_ERR_OK)){
-	$resultfilename = "resultImageTmp";
+	$resultfilename = 'resultImageTmp';
+	$mainfile = 'mainfile_tmp.jpg';
+	$stampfile = 'stampfile_tmp.jpg';
 	
 	  $file_name_main = $_FILES['mainFile']['name'];
 	  $file_tmp_main =$_FILES['mainFile']['tmp_name'];
@@ -15,14 +17,43 @@ if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile']['error'] == UPLOAD_ER
 //new size
 $percent = 0.3;
 
-//load files from server 
-$mainfilePath = "uploads/".$file_name_main;
-$stampResize = "uploads/".$file_name_copyright;
 
-$image = imagecreatefromjpeg("uploads/".$file_name_main);
-$stamp = imagecreatefromjpeg("uploads/".$file_name_copyright);
+$mainfilePathUnconverted = "uploads/".$file_name_main;
+$stampResizedUnconverted = "uploads/".$file_name_copyright;
 
-list($width, $height) = getimagesize($stampResize);
+//convert pictures to jpeg 
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once __DIR__ . '/vendor/autoload.php';
+} else {
+	require_once __DIR__ . '/php-image-converter-master/src/ImageConverter.php';
+}
+		
+if(strpos($file_name_main, '.jpg') == false) {
+	$from =  $mainfilePathUnconverted;
+	$to = 'uploads/'.$mainfile;
+	\ImageConverter\convert($from, $to, 100);
+
+	$mainfilePathConverted = 'uploads/'.$mainfile;
+	unlink($mainfilePathUnconverted);
+} else {
+	$mainfilePathConverted = $mainfilePathUnconverted;
+}
+
+if(strpos($file_name_copyright, '.jpg') == false) {
+	$from = $stampResizedUnconverted;
+	$to = 'uploads/'.$stampfile;
+	\ImageConverter\convert($from, $to, 100);
+
+	$stampResizedConverted ='uploads/'.$stampfile;
+	unlink($stampResizedUnconverted);
+} else {
+	$stampResizedConverted = $stampResizedUnconverted;
+}
+
+//resize stamp
+$stamp = imagecreatefromjpeg($stampResizedConverted);
+
+list($width, $height) = getimagesize($stampResizedConverted);
 $newwidth = $width * $percent;
 $newheight = $height * $percent;
 
@@ -34,12 +65,14 @@ $x = imagesx($thumb);
 $y = imagesy($thumb);
 
 //Put watermark on image
+$image = imagecreatefromjpeg($mainfilePathConverted);
+
 imagecopymerge($image, $thumb, imagesx($image) - $x, imagesy($image) - $y, 0, 0, imagesx($thumb), imagesy($thumb), 50);
 
 imagejpeg($image, 'uploads/'.$resultfilename.'.jpg');
 
-unlink($mainfilePath);
-unlink($stampResize);
+unlink($mainfilePathConverted);
+unlink($stampResizedConverted);
 
 //Get new image and view it
 $resultfilePath = "uploads/".$resultfilename.'.jpg';
@@ -49,7 +82,6 @@ echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
 		echo '<img src="' . $resultfilePath . '" width="200" alt="' .  pathinfo($resultfilePath, PATHINFO_FILENAME) .'">';
 		echo '<p><a href="download.php?file=' . urlencode($resultfilePath) . '">Download Image</a></p>';
 	echo '</div>';
-
 }
 
 ?>
