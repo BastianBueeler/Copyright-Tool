4c3411217d3df37901b2b07382e21a7a6b43a6e3
Bastian Bueeler <bastian.bueeler@gmail.com>
2020-10-17 21:33:34 +0200

    new comments and new responsivity

--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?php
-//Fileupload
+//Is not file empty load picutres and save it
 if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile']['error'] == UPLOAD_ERR_OK)){
 	$resultfilename = "resultImageTmp";
 	
@@ -12,9 +12,10 @@ if(!empty($_FILES['mainFile'] and $_FILES['copyrightFile']['error'] == UPLOAD_ER
 	  move_uploaded_file($file_tmp_main,"uploads/".$file_name_main);
 	  move_uploaded_file($file_tmp_copyright,"uploads/".$file_name_copyright);
 
-//calculate new size
+//new size
 $percent = 0.3;
 
+//load files from server 
 $mainfilePath = "uploads/".$file_name_main;
 $stampResize = "uploads/".$file_name_copyright;
 
@@ -40,9 +41,11 @@ imagejpeg($image, 'uploads/'.$resultfilename.'.jpg');
 unlink($mainfilePath);
 unlink($stampResize);
 
+//Get new image and view it
 $resultfilePath = "uploads/".$resultfilename.'.jpg';
-    
-	echo '<div class="img-box">';
+
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+	echo '<div class="img-box px-5 d-flex ml-auto">';
 		echo '<img src="' . $resultfilePath . '" width="200" alt="' .  pathinfo($resultfilePath, PATHINFO_FILENAME) .'">';
 		echo '<p><a href="download.php?file=' . urlencode($resultfilePath) . '">Download Image</a></p>';
 	echo '</div>';
